447e843293aae59469567ecb0c95804333fdc8db
liam-mcallister <liammca1987@gmail.com>
2020-08-13 22:01:22 +0100

    Minor changes

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
 
-  // Adds drop shadow to navbar when user scrolls 50+px
+  // Adds drop shadow to navbar when user scrolls 1+px
   $(window).scroll(function () {
-    if ($(document).scrollTop() > 50) {
+    if ($(document).scrollTop() > 1) {
       $("nav").addClass("nav-shadow");
     } else {
       $("nav").removeClass("nav-shadow");
     }
   });
-  
+
 });
